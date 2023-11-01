b58cfaee102b167395c8595f0beae7e8059613bd
Selim419 <134213066+Selim419@users.noreply.github.com>
2023-11-01 22:53:02 +0300

    Add files via upload

---
+++ sayı-bulmaca.py
@@ -0,0 +1,15 @@
+from random import randint as ra
+
+rastgele = ra(1,100)
+
+while True:
+        tahmin = int(input("1-100 arasında bir sayı girin:>
+        if tahmin < rastgele:
+                print("rakam büyüt")
+                pass
+        elif tahmin > rastgele :
+                print("rakam küçült")
+                pass
+        else:
+                print("doğru tahmin")
+                break
